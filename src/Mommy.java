5338c3bec6fd90de6a286e40a4363e9504517146
Ya Xu <syndrome@CNsyndrome.local>
2018-08-02 22:44:08 +0800

    achieve should_return_hard_when_given_hard_with_25_percent_vowels

--- src/Mommy.java
+++ src/Mommy.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 public class Mommy {
     public String toMommy(String str){
-        if (Arrays.stream(str.split("")).anyMatch(i->"aeiou".contains(i))) {
+        if (percentage(str)>0.3 && Arrays.stream(str.split("")).anyMatch(i->"aeiou".contains(i))) {
             System.out.println("ok");
             return Arrays.stream(str.split("")).map(i-> "aeiou".contains(i)?"mommy":i).reduce("",(a, b)-> a+b);
         } else {
             return str ;
         }
+    }
+
+    private double percentage (String str){
+        return Arrays.stream(str.split("")).filter(i->"aeiou".contains(i)).count()/str.length();
     }
 }
